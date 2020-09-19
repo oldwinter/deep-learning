b62d00aa99174a6941ada0cd41ce59954ecfa350
cdd <cdd@cdd.local>
2020-09-19 16:03:22 +0800

    动态规划文件夹整理

---
+++ src/leetcode/dp/121. 买卖股票的最佳时机_test.go
@@ -0,0 +1,58 @@
+/*
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+
+注意：你不能在买入股票前卖出股票。
+
+
+
+示例 1:
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+示例 2:
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+package leetcode
+
+import (
+	"testing"
+	// . "github.com/oldwinter/deepLearning/src/leetcode/common"
+)
+
+func Test_maxProfit(t *testing.T) {
+
+}
+
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	min := prices[0]
+	prices[0] = 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-min > 0 {
+			prices[i] = prices[i] - min
+		} else {
+			min = prices[i]
+			prices[i] = 0
+		}
+	}
+	max := prices[0]
+	for _, v := range prices {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
---
+++ src/leetcode/dp/5. 最长回文子串_test.go
@@ -0,0 +1,58 @@
+/*
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+
+输入: "cbbd"
+输出: "bb"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+package leetcode
+
+import (
+	"testing"
+	// . "github.com/oldwinter/deepLearning/src/leetcode/common"
+)
+
+func Test_longestPalindrome(t *testing.T) {
+	t.Log(longestPalindrome("babad"))
+}
+func longestPalindrome(s string) string {
+	if s == "" {
+		return ""
+	}
+	start := 0
+	maxlen := 1
+	ss := make([][]bool, len((s)))
+	for i := 0; i < len(s); i++ {
+		ss[i] = make([]bool, len(s))
+		ss[i][i] = true
+	}
+	for i := 0; i < len(s); i++ {
+
+		for j := 0; j < len(s); j++ {
+			if s[i] == s[j] && i+1 >= 0 && j-1 >= 0 && i+1 < len(s) && j-1 < len(s) && ss[i+1][j-1] {
+				ss[i][j] = true
+			} else {
+				ss[i][j] = false
+			}
+			if ss[i][j] {
+				if j-i+1 > maxlen {
+					maxlen = j - i + 1
+					start = i
+				}
+			}
+
+		}
+	}
+	return s[start : start+maxlen]
+
+}
